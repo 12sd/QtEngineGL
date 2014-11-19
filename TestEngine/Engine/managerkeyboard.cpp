61f6c3d378a776a97071eca910f59a8b1a3b3ade
MaxiProgram <maxiprogram@gmail.com>
2014-11-19 19:54:27 +0300

    Класс ManagerKeyboard

--- TestEngine/Engine/managerkeyboard.cpp
+++ TestEngine/Engine/managerkeyboard.cpp
@@ -10,7 +10,7 @@ ManagerKeyboard::ManagerKeyboard()
 ManagerKeyboard::~ManagerKeyboard()
 {
     buffer_key.clear();
-    qDebug()<<"~ManagerMouse";
+    qDebug()<<"~ManagerKeyboard";
 }
 
 ManagerKeyboard* ManagerKeyboard::getInstance()
@@ -24,34 +24,10 @@ void ManagerKeyboard::Update(QKeyEvent *event,bool press)
 {
     this->event = event;
     buffer_key.insert(event->key(),press);
-    /*
-     * Тестовый вариант
-    */
-//    if (event->key()==Qt::Key_Right)
-//    {
-//        buffer_key[0] = true;
-//        if (!press)
-//            buffer_key[0] = false;
-//    }else
-//    {
-//        buffer_key[0] = false;
-//    }
-//    if (event->key()==Qt::Key_Left)
-//    {
-//        buffer_key[1] = true;
-//        if (!press)
-//            buffer_key[1] = false;
-//    }else
-//    {
-//        buffer_key[1] = false;
-//    }
 }
 
 bool ManagerKeyboard::GetKey(Qt::Key key)
 {
-    /*
-     * Тестовый вариант
-    */
     if (event!=0)
     {
         return buffer_key.value(key);
